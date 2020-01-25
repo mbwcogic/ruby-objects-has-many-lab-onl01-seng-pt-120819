232524f7929f98f2db7eb541ef71cd1c94e9f79a
Michael Waldo <mbwcogic@yahoo.com>
2020-01-25 02:02:34 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,7 +7,7 @@ class Author
   def posts 
     Post.all.select {|post| post.author == self}
   end
-  def add_post(post) 
-    post  = Post.new(song_name)
-    add_song(song)
+  def add_post(post_name) 
+    post  = Post.new(post_name)
+    add_post(post)
 end 
