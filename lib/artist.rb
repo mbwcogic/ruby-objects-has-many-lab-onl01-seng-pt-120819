6dac19a35e89e8707a7f2887a090f1e1f9ee8e08
Michael Waldo <mbwcogic@yahoo.com>
2020-01-25 00:13:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,6 +19,9 @@ def add_song(song)
   def songs
     Song.all.select {|song| song.artist == self}
   end
-
+def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    add_song(song)
+  end
 
 end
